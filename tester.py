a0750d1182050ccdc5c8f5d8f5fa099864d83c46
thatrand0mgam3r <77948235+thatrand0mgam3r@users.noreply.github.com>
2021-02-19 14:51:53 -0600

    added comments

--- tester.py
+++ tester.py
@@ -1,13 +1,15 @@
 import urllib.request
+//Imports a library into the python program....will be used to add the way to find online URLs and download it
 import os
+//Imports the ability to find if a document exists in the current directory
 
 path= './http_access_log.txt'
-
+//gives the relative pathing to solve in the next step
 exists = os.path.exists(path)
+// runs a scan to check if the file is in the path we gave it
 
-print(exists)
 if exists == False:
-
+// if a false is given then the URL is searched out and the program downloads the inforation requested
     print('Starting file download......')
 
     url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
@@ -16,7 +18,8 @@ if exists == False:
     print ('Now we will run some analysis')
     file = open("http_access_log.txt")
     data = file.read()
-    occurrences = data.count("1994")
+    occurrences = data.count("1994") 
+    // counts the data with the set parameters of 1994 and is repeated for 1995 and saves them as two variables
     occurrences2 = data.count("1995")
     print('Times data was accessed last year:', occurrences)
     print('Times data was accessed in both years:', occurrences + occurrences2)
